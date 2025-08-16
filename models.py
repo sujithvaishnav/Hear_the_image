b19c98397efd5a193dc74fe7ce5eb9d4d83be2d2
sujithvaishnav <sujithvaishnavmalla@gmail.com>
2025-08-16 23:02:32 +0530

    Update models.py

--- models.py
+++ models.py
@@ -3,6 +3,6 @@ import streamlit as st
 
 @st.cache_resource
 def load_blip():
-    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large", use_fast = True)
     model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
     return processor, model
