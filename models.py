0805cb573c32c06a58b1f9e4a5a97fb5599d4a60
sujithvaishnav <sujithvaishnavmalla@gmail.com>
2025-08-16 22:58:33 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from transformers import BlipProcessor, BlipForConditionalGeneration
 
+@st.cache_resource
 def load_blip():
     processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
     model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
