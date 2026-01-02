735fcc4402b5447f1183c65e1d82f046f952416c
sujithvaishnav <sujithvaishnavmalla@gmail.com>
2026-01-02 07:46:56 +0530

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from transformers import BlipProcessor, BlipForConditionalGeneration
-import streamlit as st
-
-@st.cache_resource
-def load_blip():
-    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-large", use_fast = True)
-    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-large")
-    return processor, model
